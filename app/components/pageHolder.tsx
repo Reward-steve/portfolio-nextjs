eff27ef6439e9664ffa206424969e05d8ecbf3fa
Reward Steve <rewardstephen30@gmail.com>
2025-04-06 20:55:23 +0100

    loading

--- app/components/pageHolder.tsx
+++ app/components/pageHolder.tsx
@@ -1,5 +1,7 @@
 "use client";
 import * as m from "motion/react-client";
+import { useState } from "react";
+import { useLazyLoading } from "../hook/useLazyLoading";
 // import { useLazyLoading } from "../hook/useLazyLoading";
 // import { useContext } from "react";
 // import { lazyContext } from "../context/lazyContext";
@@ -9,13 +11,18 @@ export default function PageHolder({
 }: {
   children: React.ReactNode;
 }) {
-  <m.section
-    className="w-full flex flex-col gap-10 p-10 max-[375]:p-5 text-white rounded-xl shadow-lg"
-    initial={{ opacity: 0, y: 50 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: 0.8 }}
-    style={{ borderTop: "0.5px solid #515151" }}
-  >
-    {children}
-  </m.section>;
+  const { isLoading } = useLazyLoading();
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    <m.section
+      className="w-full flex flex-col gap-10 p-10 max-[375]:p-5 text-white rounded-xl shadow-lg"
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8 }}
+      style={{ borderTop: "0.5px solid #515151" }}
+    >
+      {children}
+    </m.section>
+  );
 }
