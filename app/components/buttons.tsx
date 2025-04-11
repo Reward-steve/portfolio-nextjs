2dc5da40d42dcf286c6d55125b36740e9cb94518
Reward Steve <rewardstephen30@gmail.com>
2025-04-11 22:11:58 +0100

    fixing

--- app/components/buttons.tsx
+++ app/components/buttons.tsx
@@ -3,16 +3,19 @@ import Link from "next/link";
 
 export default function Button({ text, path }: { text: string; path: string }) {
   return (
-    <m.a
+    <m.div
       whileHover={{ cursor: "pointer" }}
-      href={`${path}`}
-      target="_blank"
-      rel="noopener noreferrer"
       whileTap={{ scale: 0.9 }}
       transition={{ type: "spring", stiffness: 300 }}
-      className="rounded-lg border hover:text-cyan-700 dark:border-cyan-900 border-cyan-600 p-1 text-cyan-600 shadow-lg dark:hover:text-cyan-400 hover:border hover:border-cyan-400 cursor-pointer transition duration-300 easeInOut"
     >
-      {text}
-    </m.a>
+      <Link
+        href={`${path}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="rounded-lg border hover:text-cyan-700 dark:border-cyan-900 border-cyan-600 p-1 text-cyan-600 shadow-lg dark:hover:text-cyan-400 hover:border hover:border-cyan-400 cursor-pointer transition duration-300 easeInOut"
+      >
+        {text}
+      </Link>
+    </m.div>
   );
 }
