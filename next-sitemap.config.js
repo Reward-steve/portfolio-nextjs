c3b68f3f1d7cf60ad8a504f97f679b1705d689ca
Reward Steve <rewardstephen30@gmail.com>
2025-07-13 17:40:44 +0100

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://my-portfolio-website-chi-kohl.vercel.app",
+  generateRobotsTxt: true,
+  changefreq: "weekly",
+  priority: 0.7,
+  sitemapSize: 5000,
+};
